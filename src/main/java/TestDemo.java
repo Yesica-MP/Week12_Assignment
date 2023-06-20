979e9bd1476db4994b3e7528c513ad7f691576dd
Yesica-MP <yesica.lmp@gmail.com>
2023-06-20 14:48:11 -0600

    Week_12Assignment

---
+++ src/main/java/TestDemo.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class TestDemo {
+	
+	public int addPositive(int a, int b) {
+		if(a<=0 || b<=0) {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+		return a + b;
+	}
+	 public int randomNumberSquared() {
+		int number = getRandomInt();
+		return number * number;
+	}
+	 int getRandomInt() {
+		 Random random = new Random();
+		 return random.nextInt(10) + 1;
+	 }
+
+}
